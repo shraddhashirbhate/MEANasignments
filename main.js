230fd66695c3e013d0dad4c1ceafa6df12930833
shraddhashirbhate <66160661+shraddhashirbhate@users.noreply.github.com>
2020-09-05 22:21:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let mod = require("./my.module1");
+
+let add = mod.add(5, 7);
+console.log(add);
+
+let sub = mod.sub(9, 6);
+console.log(sub);
---
+++ my.module1.js
@@ -0,0 +1,18 @@
+
+let add = (a, b) => {
+    return a + b;
+}
+
+let sub = (a, b) => {
+    return a - b;
+}
+
+let mul = (a, b) => {
+    return a * b;
+}
+
+let div = (a, b) => {
+    return a / b;
+}
+
+module.exports = { add, sub, mul, div };
